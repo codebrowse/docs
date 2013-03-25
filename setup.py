97f83edca37cd914952e6fd7c648f4b6be28645c
Michael Van Veen <michael@mvanveen.net>
2013-03-25 06:09:04 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='docs',
+  version='.01',
+  description='Python Distribution Utilities',
+  author='Michael Van Veen',
+  author_email='michael@codebrow.se',
+  url='http://github.com/codebrowse/docs',
+  packages=['docs'],
+)
